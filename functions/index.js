a7e2bfc68d072ca3a2a1149afd9d9c2abfb47e9e
Rich Kevan <rskevan@me.com>
2023-10-11 14:49:55 -0700

    converted cloud function to express app

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,20 @@ const ytdl = require("ytdl-core");
 const fs = require("fs");
 const https = require("https");
 const os = require("os");
+const cors = require("cors");
+const express = require("express");
 
+const app = express();
 
-exports.youtubeDownload = onRequest(
-  {cors: ['https://localhost:5173', 'locallhost/:1']},
-  async (request, response) => {
+const corsOptions = {
+  origin:'https://richkevan.github.io/youtube_downloader/',
+  methods: ['GET'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
+const videoConvert = app.get("/", async (req, res) => {
   logger.debug(request.query);
   const vid = request.query.url.split("v=")[1];
   const video = await ytdl.getInfo(vid);
@@ -20,3 +29,5 @@ exports.youtubeDownload = onRequest(
     url: `https://www.youtube.com/embed/${vid}`,
   });
 });
+
+exports.youtubeDownload = onRequest(videoConvert);
